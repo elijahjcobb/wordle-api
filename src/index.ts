3a3f7d654ef052fa891a4d4cb1d665ea73e929ab
Elijah Cobb <elijah.cobb@vercel.com>
2022-09-30 21:55:36 -0400

    add https

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { fetchWordleOfTheDay } from "./parse";
 import * as express from "express";
 import { Cache } from "./cache";
+import * as fs from "fs";
+import * as https from "https";
 
 const app = express();
 
@@ -10,4 +12,14 @@ app.get("/", async (req, res) => {
   res.json({ solution: await cache.read() });
 });
 
-app.listen(9483);
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/wordle-api.elijahcobb.com/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/wordle-api.elijahcobb.com/fullchain.pem",
+  "utf8"
+);
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
