4fd41953fef44ff2229910cf74a28516578adf6a
Elijah Cobb <elijah.cobb@vercel.com>
2022-10-09 17:06:43 -0400

    add error

--- src/parse.ts
+++ src/parse.ts
@@ -18,46 +18,50 @@ function sleep(ms: number) {
 }
 
 export async function fetchWordleOfTheDay(): Promise<string> {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.nytimes.com/games/wordle/index.html");
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.nytimes.com/games/wordle/index.html");
 
-  await sleep(500);
-  // @ts-ignore
-  await page.$eval('[data-testid="modal-overlay"]', (elem) => elem.click());
+    await sleep(500);
+    // @ts-ignore
+    await page.$eval('[data-testid="modal-overlay"]', (elem) => elem.click());
 
-  for (let i = 0; i < 6; i++) {
-    for (const letter of LUCKY_GUESS) {
-      page.keyboard.press(letter);
-      await sleep(500);
+    for (let i = 0; i < 6; i++) {
+      for (const letter of LUCKY_GUESS) {
+        page.keyboard.press(letter);
+        await sleep(500);
+      }
+      page.keyboard.press("Enter");
+      await sleep(4000);
     }
-    page.keyboard.press("Enter");
-    await sleep(4000);
-  }
 
-  // @ts-ignore
-  const source = await page.$eval("#wordle-app-game", (el) => el.innerHTML);
-  const lastIndex = source.lastIndexOf("Toast-module_toast__");
+    // @ts-ignore
+    const source = await page.$eval("#wordle-app-game", (el) => el.innerHTML);
+    const lastIndex = source.lastIndexOf("Toast-module_toast__");
 
-  let solution = "";
-  let seenPre = false;
+    let solution = "";
+    let seenPre = false;
 
-  for (let i = lastIndex; i < source.length; i++) {
-    const char = source[i];
-    if (char === ">") {
-      seenPre = true;
-      continue;
+    for (let i = lastIndex; i < source.length; i++) {
+      const char = source[i];
+      if (char === ">") {
+        seenPre = true;
+        continue;
+      }
+      if (!seenPre) continue;
+      if (char === "<") break;
+      solution += char;
     }
-    if (!seenPre) continue;
-    if (char === "<") break;
-    solution += char;
+
+    solution = solution.toLowerCase();
+    await browser.close();
+
+    return solution;
+  } catch (e) {
+    return "crate";
   }
-
-  solution = solution.toLowerCase();
-  await browser.close();
-
-  return solution;
 }
